644b7b2ff01e73a82e4340457321e670343ceff1
yei <joaquin.estay@dasos.io>
2020-05-27 13:12:45 -0400

    0.0.0

--- sensors/dht/python/cron-sense.py
+++ sensors/dht/python/cron-sense.py
@@ -27,8 +27,17 @@ csv_temp = f"{t},{v_temp}" # String Composition
 print("Temperature Record =", csv_temp)
 
 # SENSE: Write Temperature CSV Record
+#
 # TODO: Replace "vid-000" with argument VID
-with open("/var/log/dasos/sense/vid-000/cron-sense.csv", "a") as log: # Open the file in append ('a')
+#
+# LOG: Fix, Global
+pre = time.strftime("%Y-%m-%d-")
+file = "cron-sense.csv"
+# LOG: Fix, Variable
+vid_t = "vid-000"
+path_t = "/var/log/dasos/sense/" + vid + "/" + pre + file
+print ("Path =", path_t)
+with open(path_t, "a") as log: # Open the file in append ('a')
     log.write("\n") # Append Line
     log.write(csv_temp) # Append Record
     log.close() # LOG: Close
@@ -41,15 +50,14 @@ print("Humidity Record =", csv_humi)
 #
 # TODO: Replace "vid-001" with argument VID
 #
-# LOG: Fix
-vid = "vid-001"
+# LOG: Fix, Global
+pre = time.strftime("%Y-%m-%d-")
 file = "cron-sense.csv"
-pre = time.strftime("%Y-%m-%d")
-path = "/var/log/dasos/sense/" + vid + "/" + pre + file
-print (path)
-
-#LOG: OPEN
-with open(path, "a") as log: # Open the file in append ('a')
+# LOG: Fix, Variable
+vid_h = "vid-001"
+path_h = "/var/log/dasos/sense/" + vid + "/" + pre + file
+print ("Path =", path_h)
+with open(path_h, "a") as log: # Open the file in append ('a')
     log.write("\n") # LOG: Write. Append Line
     log.write(csv_humi) # LOG: Write. Append Record
     log.close() # LOG: Close
