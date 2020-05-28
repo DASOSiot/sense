8f23adf853e6600a6f7ee44d5b2fa82d0554ac33
yei <joaquin.estay@dasos.io>
2020-05-28 12:55:36 -0400

    0.0.0

--- sensors/dht/python/cron-pulse.py
+++ sensors/dht/python/cron-pulse.py
@@ -14,18 +14,18 @@ import pandas as pd
 
 # ARGUMENTS
 script = sys.argv[0]
-print ("SCRIPT =",script,"(filename)")
+print ("SCRIPT (filename) =",script)
 vid = sys.argv[1]
-print ("VARIABLE =",vid,"(id)")
+print ("VARIABLE (vid) =",vid)
 cycle = int(sys.argv[2])
-print ("CYCLE =",cycle,"minutes")
+print ("CYCLE (minutes) =",cycle)
 
 # LOG: Fix
 log_prefix = time.strftime("%Y-%m-") # Time Prefix
 sp = "/var/log/dasos/sense/" + vid + "/" + log_prefix + "cron-sense.csv" # SP = Sense Path
 pp = "/var/log/dasos/sense/" + vid + "/" + log_prefix + "cron-pulse-" + sys.argv[2] + ".csv" # PP = Pulse Path
-print ("SENSE Log Path =",sp)
-print ("PULSE Log Path =",pp)
+print ("SENSE (path) =",sp)
+print ("PULSE (path) =",pp)
 
 # DATAFRAME: Pulse Log. Read, Write, Close.
 pulse = pd.read_csv(sp)
@@ -36,22 +36,22 @@ pulse.columns = ['t', 'v']
 
 # PULSE: Variables
 p_time = pulse["t"].mean()
-print("Time =", p_time)
+print("TIME (unix ms) =", p_time)
 
 p_mean = pulse["v"].mean()
-print("Mean =", p_mean)
-
-p_count = pulse["v"].count()
-print("Count =", p_count)
+print("MEAN =", p_mean)
 
 p_std = pulse["v"].std()
 print("STD =", p_std)
 
 p_min = pulse["v"].min()
-print("Min =", p_min)
+print("MIN =", p_min)
 
 p_max = pulse["v"].max()
-print("Max =", p_max)
+print("MAX =", p_max)
+
+p_count = pulse["v"].count()
+print("COUNT (records) =", p_count)
 
 # PULSE: CSV Record
 csv = f"{p_time},{p_mean},{p_std},{p_min},{p_max},{p_count}"
